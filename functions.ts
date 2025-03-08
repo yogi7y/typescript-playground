14f161256cfb547a2246dd38af7dadecbff6788f
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2025-03-08 11:17:12 +0530

    function with default paramaters

--- functions.ts
+++ functions.ts
@@ -1,5 +1,13 @@
-function greet(name: string) {
+function greet(name: string = 'stranger') {
     return `Hey, ${name}!`;
 }
 
-console.log(greet('Yogi'))
+function sum(num1: number, num2: number) {
+    return `Sum of ${num1} and ${num2} is ${num1 + num2}`;
+}
+
+
+
+console.log(greet('Yogi'));
+console.log(greet());
+console.log(sum(10, 5));
