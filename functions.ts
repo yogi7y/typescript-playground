6a30497d6863e37d2a4705800b251b9a88568ce0
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2025-03-08 11:18:10 +0530

    format code

--- functions.ts
+++ functions.ts
@@ -1,13 +1,11 @@
-function greet(name: string = 'stranger'): string {
-    return `Hey, ${name}!`;
+function greet(name: string = "stranger"): string {
+  return `Hey, ${name}!`;
 }
 
 function sum(num1: number, num2: number): string {
-    return `Sum of ${num1} and ${num2} is ${num1 + num2}`;
+  return `Sum of ${num1} and ${num2} is ${num1 + num2}`;
 }
 
-
-
-console.log(greet('Yogi'));
+console.log(greet("Yogi"));
 console.log(greet());
 console.log(sum(10, 5));
