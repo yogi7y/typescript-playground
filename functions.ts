543cf2b49cba3996820730671c5f1280095b337f
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2025-03-08 11:17:43 +0530

    function with return type defined

--- functions.ts
+++ functions.ts
@@ -1,8 +1,8 @@
-function greet(name: string = 'stranger') {
+function greet(name: string = 'stranger'): string {
     return `Hey, ${name}!`;
 }
 
-function sum(num1: number, num2: number) {
+function sum(num1: number, num2: number): string {
     return `Sum of ${num1} and ${num2} is ${num1 + num2}`;
 }
 
