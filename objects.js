fcf92fad9708136fa7e1356ffe7c5d6c5c827bdd
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2025-03-09 12:31:27 +0530

    type alias

--- objects.js
+++ objects.js
@@ -1,2 +1,10 @@
+// declare and set
 var locationObj = { lat: 1, lng: 2 };
 console.log(locationObj);
+var displayUser = function (user) {
+    console.log("".concat(user.name, " is ").concat(user.age, " years old."));
+};
+displayUser({
+    name: "John",
+    age: 10,
+});
--- objects.ts
+++ objects.ts
@@ -2,3 +2,17 @@
 let locationObj: { lat: number; lng: number } = { lat: 1, lng: 2 };
 
 console.log(locationObj);
+
+type User = {
+  name: string;
+  age: number;
+};
+
+const displayUser = (user: User) => {
+  console.log(`${user.name} is ${user.age} years old.`);
+};
+
+displayUser({
+  name: "John",
+  age: 10,
+});
